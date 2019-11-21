fdd7a4e7072ad9a405236d3fcfa6c397914a5efa
hwipl <33433250+hwipl@users.noreply.github.com>
2019-11-21 14:26:28 +0100

    add setup.py
    Add initial version of setup.py.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+"""
+nuqql-based setup file
+"""
+
+from setuptools import setup
+
+VERSION = "0.1"
+DESCRIPTION = "Dummy network daemon for nuqql"
+with open("README.md", 'r') as f:
+    LONG_DESCRIPTION = f.read()
+CLASSIFIERS = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+]
+
+setup(
+    name="nuqql-based",
+    version=VERSION,
+    description=DESCRIPTION,
+    license="MIT",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    author="hwipl",
+    author_email="nuqql-based@hwipl.net",
+    url="https://github.com/hwipl/nuqql-based",
+    packages=["nuqql_based"],
+    entry_points={
+        "console_scripts": ["nuqql-based = nuqql_based.based:main"]
+    },
+    classifiers=CLASSIFIERS,
+    python_requires='>=3.6',
+)
