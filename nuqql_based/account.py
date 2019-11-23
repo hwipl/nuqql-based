b7fec5ff5ca02b8f9c20e131b5e24aa265ac8a1b
hwipl <33433250+hwipl@users.noreply.github.com>
2019-11-23 20:04:13 +0100

    move accounts into account.py
    Add an "account.py" module and move accounts into it.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ nuqql_based/account.py
@@ -0,0 +1,123 @@
+"""
+Nuqql-based accounts
+"""
+
+import configparser
+import stat
+import os
+
+from nuqql_based.callback import Callback, callback
+from nuqql_based.logger import LOGGERS
+from nuqql_based.config import CONFIG
+
+
+ACCOUNTS = {}
+
+
+class Account:
+    """
+    Storage for account specific information
+    """
+
+    def __init__(self, aid=0, name="", atype="dummy", user="dummy@dummy.com",
+                 password="dummy_password", status="online"):
+        self.aid = aid
+        self.name = name
+        self.type = atype
+        self.user = user
+        self.password = password
+        self.status = status
+        self.buddies = []
+        self.logger = None
+
+    def send_msg(self, user, msg):
+        """
+        Send message to user. Currently, this only logs the message
+        """
+
+        # try to send message
+        callback(self.aid, Callback.SEND_MESSAGE, (user, msg))
+
+        # log message
+        log_msg = "message: to {0}: {1}".format(user, msg)
+        LOGGERS[self.aid].info(log_msg)
+
+
+def store_accounts():
+    """
+    Store accounts in a file.
+    """
+
+    # set accounts file and init configparser
+    accounts_file = CONFIG["dir"] / "accounts.ini"
+    config = configparser.ConfigParser()
+    config.optionxform = lambda option: option
+
+    # construct accounts config that will be written to the accounts file
+    for acc in ACCOUNTS.values():
+        section = "account {}".format(acc.aid)
+        config[section] = {}
+        config[section]["id"] = str(acc.aid)
+        config[section]["type"] = acc.type
+        config[section]["user"] = acc.user
+        config[section]["password"] = acc.password
+
+    try:
+        with open(accounts_file, "w") as acc_file:
+            # make sure only user can read/write file before storing anything
+            os.chmod(accounts_file, stat.S_IRUSR | stat.S_IWUSR)
+
+            # write accounts to file
+            config.write(acc_file)
+    except (OSError, configparser.Error) as error:
+        error_msg = "Error storing accounts file: {}".format(error)
+        LOGGERS["main"].error(error_msg)
+
+
+def load_accounts():
+    """
+    Load accounts from a file.
+    """
+
+    # make sure path and file exist
+    CONFIG["dir"].mkdir(parents=True, exist_ok=True)
+    os.chmod(CONFIG["dir"], stat.S_IRWXU)
+    accounts_file = CONFIG["dir"] / "accounts.ini"
+    if not accounts_file.exists():
+        return
+
+    # make sure only user can read/write file before using it
+    os.chmod(accounts_file, stat.S_IRUSR | stat.S_IWUSR)
+
+    # read config file
+    try:
+        config = configparser.ConfigParser()
+        config.read(accounts_file)
+    except configparser.Error as error:
+        error_msg = "Error loading accounts file: {}".format(error)
+        LOGGERS["main"].error(error_msg)
+
+    for section in config.sections():
+        # try to read account from account file
+        try:
+            acc_id = int(config[section]["id"])
+            acc_type = config[section]["type"]
+            acc_user = config[section]["user"]
+            acc_pass = config[section]["password"]
+        except KeyError as error:
+            error_msg = "Error loading account: {}".format(error)
+            LOGGERS["main"].error(error_msg)
+            continue
+
+        # add account
+        new_acc = Account(aid=acc_id, atype=acc_type, user=acc_user,
+                          password=acc_pass)
+        ACCOUNTS[new_acc.aid] = new_acc
+
+
+def get_accounts():
+    """
+    Helper for getting the accounts
+    """
+
+    return ACCOUNTS
--- nuqql_based/based.py
+++ nuqql_based/based.py
@@ -5,21 +5,19 @@ Basic nuqql backend
 """
 
 import socketserver
-import configparser
 import select
 import stat
 import html
 import sys
 import os
 
+from nuqql_based.account import ACCOUNTS, Account, store_accounts, \
+        load_accounts, get_accounts
 from nuqql_based.logger import LOGGERS, init_logger, init_main_logger, \
         init_account_loggers
 from nuqql_based.config import CONFIG, init_config
 from nuqql_based.callback import Callback, callback
 from nuqql_based.message import Format
-
-
-ACCOUNTS = {}
 
 
 class Buddy:
@@ -31,35 +29,6 @@ class Buddy:
         self.name = name
         self.alias = alias
         self.status = status
-
-
-class Account:
-    """
-    Storage for account specific information
-    """
-
-    def __init__(self, aid=0, name="", atype="dummy", user="dummy@dummy.com",
-                 password="dummy_password", status="online"):
-        self.aid = aid
-        self.name = name
-        self.type = atype
-        self.user = user
-        self.password = password
-        self.status = status
-        self.buddies = []
-        self.logger = None
-
-    def send_msg(self, user, msg):
-        """
-        Send message to user. Currently, this only logs the message
-        """
-
-        # try to send message
-        callback(self.aid, Callback.SEND_MESSAGE, (user, msg))
-
-        # log message
-        log_msg = "message: to {0}: {1}".format(user, msg)
-        LOGGERS[self.aid].info(log_msg)
 
 
 class NuqqlBaseHandler(socketserver.BaseRequestHandler):
@@ -626,86 +595,6 @@ def run_server(config):
             run_inet_server(config)
         elif config["af"] == "unix":
             run_unix_server(config)
-
-
-def store_accounts():
-    """
-    Store accounts in a file.
-    """
-
-    # set accounts file and init configparser
-    accounts_file = CONFIG["dir"] / "accounts.ini"
-    config = configparser.ConfigParser()
-    config.optionxform = lambda option: option
-
-    # construct accounts config that will be written to the accounts file
-    for acc in ACCOUNTS.values():
-        section = "account {}".format(acc.aid)
-        config[section] = {}
-        config[section]["id"] = str(acc.aid)
-        config[section]["type"] = acc.type
-        config[section]["user"] = acc.user
-        config[section]["password"] = acc.password
-
-    try:
-        with open(accounts_file, "w") as acc_file:
-            # make sure only user can read/write file before storing anything
-            os.chmod(accounts_file, stat.S_IRUSR | stat.S_IWUSR)
-
-            # write accounts to file
-            config.write(acc_file)
-    except (OSError, configparser.Error) as error:
-        error_msg = "Error storing accounts file: {}".format(error)
-        LOGGERS["main"].error(error_msg)
-
-
-def load_accounts():
-    """
-    Load accounts from a file.
-    """
-
-    # make sure path and file exist
-    CONFIG["dir"].mkdir(parents=True, exist_ok=True)
-    os.chmod(CONFIG["dir"], stat.S_IRWXU)
-    accounts_file = CONFIG["dir"] / "accounts.ini"
-    if not accounts_file.exists():
-        return
-
-    # make sure only user can read/write file before using it
-    os.chmod(accounts_file, stat.S_IRUSR | stat.S_IWUSR)
-
-    # read config file
-    try:
-        config = configparser.ConfigParser()
-        config.read(accounts_file)
-    except configparser.Error as error:
-        error_msg = "Error loading accounts file: {}".format(error)
-        LOGGERS["main"].error(error_msg)
-
-    for section in config.sections():
-        # try to read account from account file
-        try:
-            acc_id = int(config[section]["id"])
-            acc_type = config[section]["type"]
-            acc_user = config[section]["user"]
-            acc_pass = config[section]["password"]
-        except KeyError as error:
-            error_msg = "Error loading account: {}".format(error)
-            LOGGERS["main"].error(error_msg)
-            continue
-
-        # add account
-        new_acc = Account(aid=acc_id, atype=acc_type, user=acc_user,
-                          password=acc_pass)
-        ACCOUNTS[new_acc.aid] = new_acc
-
-
-def get_accounts():
-    """
-    Helper for getting the accounts
-    """
-
-    return ACCOUNTS
 
 
 def main():
