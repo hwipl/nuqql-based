13f14ff3793e9b2d28d1aef1bc7ea0689c30c9e5
hwipl <33433250+hwipl@users.noreply.github.com>
2019-12-19 15:08:33 +0100

    setup.py: read version from main.py
    In setup.py, read the nuqql-based version from nuqql_based/main.py.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -4,9 +4,13 @@
 nuqql-based setup file
 """
 
+import os
+import re
+import codecs
+
 from setuptools import setup
 
-VERSION = "0.1"
+# setup parameters
 DESCRIPTION = "Dummy network daemon for nuqql"
 with open("README.md", 'r') as f:
     LONG_DESCRIPTION = f.read()
@@ -15,9 +19,35 @@ CLASSIFIERS = [
     "License :: OSI Approved :: MIT License",
 ]
 
+
+# setup helpers
+def read(*parts):
+    """
+    Read encoded file
+    """
+
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, *parts), 'r') as enc_file:
+        return enc_file.read()
+
+
+def find_version(*file_paths):
+    """
+    Find version in encoded file
+    """
+
+    version_file = read(*file_paths)
+    version_pattern = r"^VERSION = ['\"]([^'\"]*)['\"]"
+    version_match = re.search(version_pattern, version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+# run setup
 setup(
     name="nuqql-based",
-    version=VERSION,
+    version=find_version("nuqql_based", "main.py"),
     description=DESCRIPTION,
     license="MIT",
     long_description=LONG_DESCRIPTION,
