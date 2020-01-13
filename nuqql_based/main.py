95f5a6b5b7e57d472206ccbef7fcc41d1ecd9562
hwipl <33433250+hwipl@users.noreply.github.com>
2020-01-13 16:24:27 +0100

    add status callbacks to main.py
    Add status callbacks to the based testing implementation in main.py.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- nuqql_based/main.py
+++ nuqql_based/main.py
@@ -4,9 +4,41 @@
 Basic nuqql backend main entry point
 """
 
+from typing import TYPE_CHECKING, Callable, List, Optional, Tuple
+
 from nuqql_based.based import Based
+from nuqql_based.callback import Callback
+from nuqql_based.message import Message
+
+if TYPE_CHECKING:   # imports for typing
+    from nuqql_based.account import Account     # noqa
+
+CallbackFunc = Callable[[Optional["Account"], Callback, Tuple], str]
 
 VERSION = "0.1"
+
+
+def set_status(acc: Optional["Account"], _cmd: Callback, params: Tuple) -> str:
+    """
+    Set the status of the account
+    """
+
+    assert acc
+    status = params[0]
+    acc.status = status
+    acc.receive_msg(Message.status(acc, status))
+    return ""
+
+
+def get_status(acc: Optional["Account"], _cmd: Callback,
+               _params: Tuple) -> str:
+    """
+    Get the status of the account
+    """
+
+    assert acc
+    acc.receive_msg(Message.status(acc, acc.status))
+    return ""
 
 
 def main() -> None:
@@ -15,6 +47,11 @@ def main() -> None:
     """
 
     based = Based("based", VERSION)
+    callbacks: List[Tuple[Callback, CallbackFunc]] = [
+        (Callback.SET_STATUS, set_status),
+        (Callback.GET_STATUS, get_status),
+    ]
+    based.set_callbacks(callbacks)
     based.start()
 
 
