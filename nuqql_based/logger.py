fb326185b5578ded588e47b5dc059d7509068426
hwipl <33433250+hwipl@users.noreply.github.com>
2020-05-31 16:21:10 +0200

    fix logging.basicConfig() parameters for python version < 3.8
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- nuqql_based/logger.py
+++ nuqql_based/logger.py
@@ -5,6 +5,7 @@ Nuqql-based logging
 import logging
 import stat
 import os
+import sys
 
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:   # imports for typing
@@ -28,8 +29,19 @@ def init(config: "Config") -> None:
     fmt = "%(asctime)s %(levelname)-5.5s %(message)s"
     date_fmt = "%s"
     loglevel = config.get_loglevel()
-    logging.basicConfig(filename=file_name, format=fmt, datefmt=date_fmt,
-                        level=loglevel, force=True)
+
+    if sys.version_info >= (3, 8):
+        logging.basicConfig(filename=file_name, format=fmt, datefmt=date_fmt,
+                            level=loglevel, force=True)
+    else:
+        # logging.basicConfig() does not have the "force" parameter before 3.8
+        # so remove handlers if necessary and call it without force
+        root_logger = logging.getLogger()
+        if root_logger.handlers:
+            for handler in root_logger.handlers:
+                root_logger.removeHandler(handler)
+        logging.basicConfig(filename=file_name, format=fmt, datefmt=date_fmt,
+                            level=loglevel)
 
     # restrict log file access
     os.chmod(file_name, stat.S_IRUSR | stat.S_IWUSR)
